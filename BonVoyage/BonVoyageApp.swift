82a2736320fd0190cdc556f26b1e92b4cf2f8009
JasmineAufiero <jasmineaufiero@gmail.com>
2024-10-13 21:11:38 +0200

    Initial Commit

---
+++ BonVoyage/BonVoyageApp.swift
@@ -0,0 +1,17 @@
+//
+//  BonVoyageApp.swift
+//  BonVoyage
+//
+//  Created by Jasmine Aufiero on 13/10/24.
+//
+
+import SwiftUI
+
+@main
+struct BonVoyageApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BonVoyageTests/BonVoyageTests.swift
@@ -0,0 +1,17 @@
+//
+//  BonVoyageTests.swift
+//  BonVoyageTests
+//
+//  Created by Jasmine Aufiero on 13/10/24.
+//
+
+import Testing
+@testable import BonVoyage
+
+struct BonVoyageTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
