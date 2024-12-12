933473b6c50fcfd74660afabea99af9de529e216
JasmineAufiero <jasmineaufiero@gmail.com>
2024-12-12 10:46:45 +0100

    Aggiunta Page Control

---
+++ BonVoyage/View/Cards/PageControl.swift
@@ -0,0 +1,18 @@
+//
+//  PageControl.swift
+//  BonVoyage
+//
+//  Created by Jasmine Aufiero on 12/12/24.
+//
+
+import SwiftUI
+
+struct PageControl: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    PageControl()
+}
